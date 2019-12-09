9cee685474c4121859a3567d0c96c87d6a343793
Vishnu Vardhan Dhanabalan <vishnudhanabalan@outlook.com>
2019-12-09 23:55:54 +0530

    REFACTOR: moved app component to index.tsx level

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import HomePage from "../pages/HomePage";
+import HomePage from "./pages/HomePage";
 
 class App extends React.Component<{}, {}> {
   render(): JSX.Element {
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 import "./styles/fonts/fonts.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
