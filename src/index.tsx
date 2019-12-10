dd8f95234e60a463966fefbb1f3fa347bf0c212a
Vishnu Dhanabalan <vishnudhanabalan@outlook.com>
2019-12-10 21:13:56 +0530

    FIX: wrong path of App component in the hot module replacement statement

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ ReactDOM.render(<App />, document.getElementById("app"));
 declare let module: { hot: any };
 
 if (module.hot) {
-  module.hot.accept("./components/App", () => {
-    const NewApp = require("./components/App").default;
+  module.hot.accept("./App", () => {
+    const NewApp = require("./App").default;
 
     ReactDOM.render(<NewApp />, document.getElementById("app"));
   });
