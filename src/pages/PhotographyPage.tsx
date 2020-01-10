1f8eadb8db039e59c28ddd0eda4e7c2c9431b7cb
Vishnu Dhanabalan <vishnudhanabalan@outlook.com>
2020-01-11 01:49:26 +0530

    Added temporary messages

---
+++ src/pages/PhotographyPage.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import styled from "styled-components";
+
+import Header from "../components/Header";
+
+const Container = styled.div`
+  font-family: BitterRegular;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #f5f5f5;
+  font-size: 20px;
+  min-height: 100vh;
+`;
+
+const PageUnderConstruction = styled.div`
+  margin: 0 auto;
+  font-size: 25px;
+`;
+
+const PhotographyPage: React.SFC<any> = (props: any) => {
+  return (
+    <Container>
+      <Header showHome={true} />
+      <PageUnderConstruction>
+        Oops, this page is still under construction. Please comeback later. 🙏
+      </PageUnderConstruction>
+    </Container>
+  );
+};
+
+export default PhotographyPage;
