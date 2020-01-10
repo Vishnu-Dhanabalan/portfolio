db71d6b095155a7b058a3e22f26581f813b68b0a
Vishnu Dhanabalan <vishnudhanabalan@outlook.com>
2020-01-11 01:57:45 +0530

    revert back to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
