741164456ba84f1aee04dc22dc0e4e401d3cf602
Vishnu Dhanabalan <vishnudhanabalan@outlook.com>
2020-01-11 01:48:31 +0530

    Added blog and photography pages, changed HashRouter to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import styled from "styled-components";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import DetailedBioPage from "./pages/DetailedBioPage";
+import BlogPage from "./pages/BlogPage";
+import PhotographyPage from "./pages/PhotographyPage";
 
 // Use this to declare global styles for this
 const AppContainer = styled.div`
@@ -31,6 +33,8 @@ class App extends React.Component<{}, {}> {
       <Router>
         <Switch>
           <Route exact path="/" component={AppLandingPage} />
+          <Route path="/blog" component={BlogPage} />
+          <Route path="/photography" component={PhotographyPage} />
         </Switch>
       </Router>
     );
